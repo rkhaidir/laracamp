1631ef88d287cbaae167f99fcc743dd50bf5fcb5
rkhaidir <rkhaidir085@gmail.com>
2024-01-31 01:18:52 +0800

    install and integration socialite

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -21,14 +21,36 @@
                   <a class="nav-link" href="#">Business</a>
               </li>
           </ul>
+          @auth
+          <div class="d-flex user-logged nav-item dropdown no-arrow">
+            <a href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                Halo, {{ Auth::user()->name }}
+                <img src="{{ Auth::user()->avatar }}" class="user-photo" alt="">
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="right: 0; left: auto;">
+                    <li>
+                        <a class="dropdown-item" href="#">My Dashboard</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sign Out</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+
+                        </form>
+                    </li>
+                </ul>
+            </a>
+        </div>
+          @else
           <div class="d-flex">
-              <a href="{{ route('login') }}" class="btn btn-master btn-secondary me-3">
-                  Sign In
-              </a>
-              <a href="{{ route('login') }}" class="btn btn-master btn-primary">
-                  Sign Up
-              </a>
-          </div>
+            <a href="{{ route('login') }}" class="btn btn-master btn-secondary me-3">
+                Sign In
+            </a>
+            <a href="{{ route('login') }}" class="btn btn-master btn-primary">
+                Sign Up
+            </a>
+        </div>
+          @endauth
+          
       </div>
   </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +24,11 @@ Route::get('checkout', function () {
 Route::get('success-checkout', function () {
     return view('success_checkout');
 })->name('success-checkout');
+
+
+// socialite
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
